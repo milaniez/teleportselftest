ef7a88dc1561d0a081ac8dae53269573c6446991
Mehdi Zeinali <zeinali8@gmail.com>
2021-07-26 20:44:06 -0700

    init commit for util/mtls

---
+++ util/mtls/go.mod
@@ -0,0 +1,5 @@
+module github.com/milaniez/teleportselftest/util/mtls
+
+go 1.16
+
+require google.golang.org/grpc v1.39.0
---
+++ util/mtls/mtls.go
@@ -0,0 +1,62 @@
+package mtls
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"google.golang.org/grpc/credentials"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+func GetTLSConf(certFile, keyFile, caDir string, isServer bool) (credentials.TransportCredentials, error) {
+	if !strings.HasSuffix(caDir, "/") {
+		caDir += "/"
+	}
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		return nil, err
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPoolMemCnt := 0
+	caDirFiles, err := ioutil.ReadDir(caDir)
+	if err != nil {
+		return nil, err
+	}
+	for _, entry := range caDirFiles {
+		if entry.IsDir() || !strings.HasSuffix(entry.Name(), ".pem") {
+			continue
+		}
+		caFilePath := caDir + entry.Name()
+		caFile, err := ioutil.ReadFile(caFilePath)
+		if err != nil {
+			return nil, err
+		}
+		if !caCertPool.AppendCertsFromPEM(caFile) {
+			log.Printf("Warning: Certificate %v not added.\n", caFilePath)
+		} else {
+			caCertPoolMemCnt++
+		}
+	}
+	if caCertPoolMemCnt == 0 {
+		return nil, errors.New("no CA certificate")
+	}
+	tlsConfig := &tls.Config{
+		ClientCAs:  caCertPool,
+		VerifyPeerCertificate: func(_ [][]byte, verifiedChains [][]*x509.Certificate) error {
+			for _, chain := range verifiedChains {
+				if len(chain) > 2 || len(chain) == 0 {
+					continue
+				}
+				return nil
+			}
+			return errors.New("invalid certificate chains")
+		},
+		Certificates: []tls.Certificate{cert},
+	}
+	if isServer {
+		tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
+	}
+	return credentials.NewTLS(tlsConfig), nil
+}
